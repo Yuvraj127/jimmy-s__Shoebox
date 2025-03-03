92a6f625191d84b302ba89b3ae01ee6f11014372
Yuvraj127 <yuvrajsinh0106@gmail.com>
2025-03-03 08:09:41 -0500

    enabled block editing for product

--- themes/Jimmy-theme/inc/woocommerce.php
+++ themes/Jimmy-theme/inc/woocommerce.php
@@ -1,10 +1,9 @@
 <?php
-/**
- * This file has been left empty on purpose.
- *
- * @link https://core.trac.wordpress.org/ticket/54272
- *
- * @package no-code-theme
- * @since 1.0.0
- */
-require_once get_theme_file_path('inc/woocommerce.php')
+// enable gutenberg for woocommerce
+function jimmy_shoebox_activate_gutenberg_product( $can_edit, $post_type ) {
+    if ( $post_type === 'product' ) {
+        $can_edit = true;
+    }
+    return $can_edit;
+}
+add_filter( 'use_block_editor_for_post_type', 'jimmy_shoebox_activate_gutenberg_product', 10, 2 );

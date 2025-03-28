1562c9bf287f91d8a502b586c5db8e21a2bf1e36
Yuvraj127 <yuvrajsinh0106@gmail.com>
2025-03-28 13:48:34 -0400

    limited edition post type created

--- plugins/shoecare_pt/shoebox_announcement_pt.php
+++ plugins/shoecare_pt/shoebox_announcement_pt.php
@@ -122,3 +122,63 @@ function shoe_review_pt() {
 
 }
 
+add_action( 'init', 'limited_edition_pt' );
+
+function limited_edition_pt() {
+
+   $labels = array(
+
+      'name'                     => __( 'Announcements', 'limited edition' ),
+      'singular_name'            => __( 'Announcement', 'limited edition' ),
+      'add_new'                  => __( 'Add New', 'limited edition' ),
+      'add_new_item'             => __( 'Add New Announcement', 'limited edition' ),
+      'edit_item'                => __( 'Edit Announcement', 'limited edition' ),
+      'new_item'                 => __( 'New Announcement', 'limited edition' ),
+      'view_item'                => __( 'View Announcement', 'limited edition' ),
+      'view_items'               => __( 'View Announcements', 'limited edition' ),
+      'search_items'             => __( 'Search Announcements', 'limited edition' ),
+      'not_found'                => __( 'No Announcements found.', 'limited edition' ),
+      'not_found_in_trash'       => __( 'No Announcements found in Trash.', 'limited edition' ),
+      'parent_item_colon'        => __( 'Parent Announcements:', 'limited edition' ),
+      'all_items'                => __( 'All Announcements', 'limited edition' ),
+      'archives'                 => __( 'Announcement Archives', 'limited edition' ),
+      'attributes'               => __( 'Announcement Attributes', 'limited edition' ),
+      'insert_into_item'         => __( 'Insert into Announcement', 'limited edition' ),
+      'uploaded_to_this_item'    => __( 'Uploaded to this Announcement', 'limited edition' ),
+      'featured_image'           => __( 'Featured Image', 'limited edition' ),
+      'set_featured_image'       => __( 'Set featured image', 'limited edition' ),
+      'remove_featured_image'    => __( 'Remove featured image', 'limited edition' ),
+      'use_featured_image'       => __( 'Use as featured image', 'limited edition' ),
+      'menu_name'                => __( 'Announcements', 'limited edition' ),
+      'filter_items_list'        => __( 'Filter Announcement list', 'limited edition' ),
+      'filter_by_date'           => __( 'Filter by date', 'limited edition' ),
+      'items_list_navigation'    => __( 'Announcements list navigation', 'limited edition' ),
+      'items_list'               => __( 'Announcements list', 'limited edition' ),
+      'item_published'           => __( 'Announcement published.', 'limited edition' ),
+      'item_published_privately' => __( 'Announcement published privately.', 'limited edition' ),
+      'item_reverted_to_draft'   => __( 'Announcement reverted to draft.', 'limited edition' ),
+      'item_scheduled'           => __( 'Announcement scheduled.', 'limited edition' ),
+      'item_updated'             => __( 'Announcement updated.', 'limited edition' ),
+      'item_link'                => __( 'Announcement Link', 'limited edition' ),
+      'item_link_description'    => __( 'A link to an announcement.', 'limited edition' ),
+
+   );
+
+   $args = array(
+
+      'labels'                => $labels,
+      'public'                => true,
+      'has_archive'           => true,
+      'show_in_rest'          => true,
+      'menu_icon'             => 'dashicons-megaphone',
+      'capability_type'       => 'post',
+      'capabilities'          => array(),
+      'supports'              => array( 'title', 'editor', 'revisions' ),
+      'rewrite'               => array( 'slug' => 'limited edition' ),
+   );
+
+   register_post_type( 'limited_edition_pt', $args );
+
+}
+
+
